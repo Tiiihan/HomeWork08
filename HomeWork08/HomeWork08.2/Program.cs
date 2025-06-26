519dd69d74397ba3a142c6942958bb2a2cf989e3
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-26 17:15:27 +0300

    Resolved task 2

--- HomeWork08/HomeWork08.2/Program.cs
+++ HomeWork08/HomeWork08.2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//2. Визначити, чи вірно, що сума елементів масиву з пункту 1 є невід'ємне число.
+int[] arr = new int[10];
+const int MIN = -10;
+const int MAX = 10;
+var random = new Random();
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+	arr[i] = random.Next(MIN, MAX);
+	Console.WriteLine(arr[i]);
+	sum += arr[i];
+}
+
+if (sum > 0)
+	Console.WriteLine($"Sum of all array elements is greater than 0: {sum}");
+else Console.WriteLine($"Sum of all array elements is less than 0: {sum}");
